ce295ec9d8c542830adfb3727b16a909a55fb0c6
goretante <agoret00@fesb.hr>
2025-11-06 18:45:51 +0100

    feat: add Progress Reports page with chart listings

---
+++ frontend/app/dashboard/progress-reports/page.tsx
@@ -0,0 +1,17 @@
+import PageWrapper from "../../../components/PageWrapper";
+import Link from "next/link";
+
+export default function ProgressReportsPage() {
+  return (
+    <PageWrapper title="Progress Reports">
+      <ul>
+        <li>
+          Chart 1
+        </li>
+        <li>
+          Chart 2
+        </li>
+      </ul>
+    </PageWrapper>
+  );
+}
